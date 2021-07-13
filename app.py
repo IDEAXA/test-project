b5ec046568ba813e8aaa5c9d64ac8fd96fed85d6
IDEAXA <59970605+IDEAXA@users.noreply.github.com>
2021-07-14 01:30:58 +0530

    CI-CD pipeline created

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route('/')
-def index():
-    return "my name is python"
+def index0():
+    return "0"
+
+
+@app.route('/1')
+def index1():
+    return "1"
+
+
+@app.route('/2')
+def index2():
+    return "2"
+
+
+@app.route('/3')
+def index3():
+    return "3"
+
+
+@app.route('/4')
+def index4():
+    return "4"
+
 
 if __name__ == '__main__':
     app.run()
