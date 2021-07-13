bff6d53eb6d468e19ed99e5a4df280da10c4c513
IDEAXA <59970605+IDEAXA@users.noreply.github.com>
2021-07-14 01:46:45 +0530

    updates

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index0():
-    return "0"
+def index():
+    return "HELLO"
 
 
-@app.route('/1')
-def index1():
-    return "1"
-
-
-@app.route('/2')
-def index2():
-    return "2"
-
-
-@app.route('/3')
-def index3():
-    return "3"
-
-
-@app.route('/4')
-def index4():
-    return "4"
+# @app.route('/1')
+# def index1():
+#     return "1"
+#
+#
+# @app.route('/2')
+# def index2():
+#     return "2"
+#
+#
+# @app.route('/3')
+# def index3():
+#     return "3"
+#
+#
+# @app.route('/4')
+# def index4():
+#     return "4"
 
 
 if __name__ == '__main__':
--- test_app.py
+++ test_app.py
@@ -1,21 +1,21 @@
-from app import index0, index1, index2, index3, index4
+from app import index #, index1, index2, index3, index4
 
 
-def test_index0():
-    assert index0() == "0"
+def test_index():
+    assert index() == "HELLO"
 
-
-def test_index1():
-    assert index1() == "1"
-
-
-def test_index2():
-    assert index2() == "2"
-
-
-def test_index3():
-    assert index3() == "3"
-
-
-def test_index4():
-    assert index4() == "4"
+#
+# def test_index1():
+#     assert index1() == "1"
+#
+#
+# def test_index2():
+#     assert index2() == "2"
+#
+#
+# def test_index3():
+#     assert index3() == "3"
+#
+#
+# def test_index4():
+#     assert index4() == "4"
