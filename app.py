d44cb7436d989fb762c76356013047f7429f9fa9
IDEAXA <59970605+IDEAXA@users.noreply.github.com>
2021-07-14 18:38:24 +0530

    updates

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
-import flask
+from flask import Flask, request, jsonify
+import json
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
 def index0():
-    return "0"
+    return json.dumps({'data': 0})
 
 
 @app.route('/1')
 def index1():
-    return "1"
+    return json.dumps({'data': 1})
 
 
 @app.route('/2')
 def index2():
-    return "2"
+    return json.dumps({'data': 2})
 
 
 @app.route('/3')
 def index3():
-    return "3"
+    return json.dumps({'data': 3})
 
 
 @app.route('/4')
 def index4():
-    return "4"
+    return json.dumps({'data': 4})
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    return jsonify({'sum': data['a'] + data['b']})
 
 
 if __name__ == '__main__':
--- test_app.py
+++ test_app.py
@@ -1,21 +1,50 @@
-from app import index0, index1, index2, index3, index4
+from flask import json
+
+from app import app
 
 
 def test_index0():
-    assert index0() == "0"
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'{"data": 0}'
 
 
 def test_index1():
-    assert index1() == "1"
+    response = app.test_client().get('/1')
+    assert response.status_code == 200
+    assert response.data == b'{"data": 1}'
 
 
 def test_index2():
-    assert index2() == "2"
+    response = app.test_client().get('/2')
+    assert response.status_code == 200
+    assert response.data == b'{"data": 2}'
 
 
 def test_index3():
-    assert index3() == "3"
+    response = app.test_client().get('/3')
+    assert response.status_code == 200
+    assert response.data == b'{"data": 3}'
 
 
 def test_index4():
-    assert index4() == "4"
+    response = app.test_client().get('/4')
+    assert response.status_code == 200
+    assert response.data == b'{"data": 4}'
+
+
+def test_add():
+    temp = {
+        "a":10,
+        "b":20
+    }
+    response = app.test_client().post(
+        '/add',
+        data=json.dumps(temp),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['sum'] == temp["a"]+temp["b"]
